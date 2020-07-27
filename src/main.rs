9ab4c01d0b95c2b59905d8ba4cd20e081bf59c28
Reed Krawiec <reedkrawiec@gmail.com>
2020-07-27 15:06:36 -0400

    Version Change Support

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,13 @@ struct Target {
     caller_id: u32,
 }
 
+#[derive(FromForm)]
+struct VersionTarget{
+    target_id: u32,
+    caller_id: u32,
+    target_version: String
+}
+
 struct ServerInstance {
     server_process: std::process::Child,
     stdout_join: Option<std::thread::JoinHandle<()>>,
@@ -31,6 +38,12 @@ struct Servers {
     refs: Mutex<HashMap<String, ServerInstance>>,
 }
 
+fn change_version(server_id:u32,target:String) -> Result<(),Error>{
+    fs::remove_file(format!("servers/{}/server.jar",server_id))?;
+    fs::copy(format!("server/{}/server.jar",target),format!("servers/{}/server.jar",server_id))?;
+    Ok(())
+}
+
 fn delete_server(server_id: u32) -> Result<(), Error> {
     fs::remove_dir_all(format!("servers/{}", server_id))?;
     Ok(())
@@ -39,10 +52,10 @@ fn delete_server(server_id: u32) -> Result<(), Error> {
 fn create_server() -> io::Result<u32> {
     let id = rand::random::<u32>();
     fs::create_dir(format!("servers/{}", id))?;
-    fs::copy("server/eula.txt", format!("servers/{}/eula.txt", id))?;
+    fs::copy("server/1.16.1/eula.txt", format!("servers/{}/eula.txt", id))?;
     fs::copy(
-        "server/minecraft_server.1.16.1.jar",
-        format!("servers/{}/minecraft_server.1.16.1.jar", id),
+        "server/1.16.1/server.jar",
+        format!("servers/{}/server.jar", id),
     )?;
     Ok(id)
 }
@@ -63,7 +76,7 @@ fn start_server(id: u32) -> Result<ServerInstance, Error> {
             "-Xmx1024M",
             "-Xms1024M",
             "-jar",
-            "minecraft_server.1.16.1.jar",
+            "server.jar",
             "nogui",
             "--port",
             &port.to_string(),
@@ -85,15 +98,28 @@ fn start_server(id: u32) -> Result<ServerInstance, Error> {
     })
 }
 
+#[post("/version", data = "<target>")]
+fn version(target: Form<VersionTarget>, servers:State<Servers>) -> String {
+    let mut map = servers.refs.lock().expect("locks");
+    if let Some(server_inst) = map.get_mut(&target.target_id.to_string()) {
+        stop_server(server_inst);
+        map.remove(&target.target_id.to_string());
+    }
+    match change_version(target.target_id, String::from(target.target_version.clone())){
+        Ok(_) => return String::from("1"),
+        Err(e) => return String::from("-1")
+    };
+} 
+
 #[post("/stop", data = "<target>")]
 fn stop(target: Form<Target>, servers: State<Servers>) -> String {
     let mut map = servers.refs.lock().expect("locks");
     if !map.contains_key(&target.target_id.to_string()) {
         return String::from("-1");
     }
-    let server = map.get_mut(&target.target_id.to_string()).unwrap();
-    let port = server.port.to_string();
-    stop_server(server);
+    let server_inst = map.get_mut(&target.target_id.to_string()).unwrap();
+    let port = server_inst.port.to_string();
+    stop_server(server_inst);
     map.remove(&target.target_id.to_string());
     String::from("1")
 }
@@ -115,13 +141,12 @@ fn start(target: Form<Target>, servers: State<Servers>) -> String {
     };
 }
 
-
 #[post("/delete", data = "<target>")]
 fn delete(target: Form<Target>, servers:State<Servers>) -> String {
     let mut map = servers.refs.lock().expect("locks");
-    if map.contains_key(&target.target_id.to_string()) {
-        let server_inst = map.get_mut(&target.target_id.to_string()).unwrap();
+    if let Some(server_inst) = map.get_mut(&target.target_id.to_string()) {
         stop_server(server_inst);
+        map.remove(&target.target_id.to_string());
     }
     match delete_server(target.target_id){
         Ok(_) => return String::from("1"),
@@ -142,6 +167,6 @@ fn main() {
         .manage(Servers {
             refs: Mutex::new(HashMap::new()),
         })
-        .mount("/", routes![create, start, stop, delete])
+        .mount("/", routes![create, start, stop, delete, version])
         .launch();
 }
