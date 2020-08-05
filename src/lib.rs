6810a9a0a011c4276a500094d4901ab9ab9d414b
Reed Krawiec <reedkrawiec@gmail.com>
2020-08-05 16:18:36 -0400

    Better Copying More efficient server internals

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,70 @@
-use rand::{thread_rng, Rng};
+//! Allows for simple control, and input / output of minecraft servers.
+//!
+//! # Examples
+//! ```
+//!use serbo;
+//!use std::io::{self};
+//!use std::error::Error;
+//!
+//!fn main() -> Result<(), Box<dyn Error>>{
+//!  let mut manager = serbo::Manager::new("servers", "server");
+//!  let port = 25565;
+//!  let id = "1";
+//!  if !manager.exists(id){
+//!    manager.create(id, "1.16.1")?;
+//!  }
+//!  loop {
+//!    let reader = io::stdin();
+//!    let mut buf = String::new();
+//!    println!("Enter your command.");
+//!    reader.read_line(&mut buf)?;
+//!    match buf.trim() {
+//!      "stop" => {
+//!        //Stops the server
+//!        println!("Server stopping.");
+//!        manager.stop(id)?;
+//!        break Ok(());
+//!      }
+//!      "start" => {
+//!        //Starts the server
+//!        println!("Server starting.");
+//!        manager.start(id, port)?;
+//!      }
+//!      "send" => {
+//!        //Prompts for a command to send to the server
+//!        let mut send_buf = String::new();
+//!        println!("Enter the command to send to the server.");
+//!        reader.read_line(&mut send_buf)?;
+//!        //Remove the newline from read_line
+//!        send_buf = send_buf[..send_buf.chars().count()-1].to_string();
+//!        let instance = manager.get(id).unwrap();
+//!        instance.send(send_buf);
+//!      },
+//!      "get" => {
+//!        //Gets the last 5 stdout lines
+//!        let instance:&serbo::Instance = manager.get(id).unwrap();
+//!        let vec = instance.get(0);
+//!        let length = vec.len();
+//!        //Create a vec from the last 5 lines
+//!        let trimmed_vec;
+//!        if length >= 5{
+//!         trimmed_vec = Vec::from(&vec[length-5..]);
+//!        }
+//!        else{
+//!          trimmed_vec = Vec::from(vec);
+//!        }
+//!        for line in trimmed_vec{
+//!          println!("{}",line);
+//!        }
+//!      },
+//!      _ => {
+//!        println!("Unrecognized command");
+//!      }
+//!    }
+//!  }
+//!}
+//! ```
+
 use std::collections::HashMap;
 use std::fs;
 use std::io;
@@ -8,40 +74,87 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use std::thread::sleep;
 use std::time;
+use std::path::Path;
+use copy_dir::{copy_dir};
 
+/// Controls the creation and deleting of servers, and whether they are currently active.
 pub struct Manager {
   servers: HashMap<String, Instance>,
-  folder: String,
+  server_files_folder: String,
   version_folder: String,
 }
 
 impl Manager {
-  pub fn new(folder: String, version_folder: String) -> Manager {
+/// Creates a new server manager
+/// # Arguments
+/// * `server_files_folder` - the folder that will hold each server's folder, which contains its server files.
+/// * `version_folder` - the folder containing the base files of servers for the MC versions that you wish to host. Used as a base to create new servers.
+/// # Examples
+/// ```
+///   let manager = serbo::Manager::new("folder1","folder2");
+/// ```
+/// # Remarks
+/// The version_folder should be a folder that contains folders that are named the same as the MC server files they contain.
+  pub fn new(server_files_folder: &str, version_folder: &str) -> Manager {
     Manager {
       servers: HashMap::new(),
-      folder: folder,
-      version_folder: version_folder,
+      server_files_folder: server_files_folder.to_string(),
+      version_folder: version_folder.to_string(),
     }
   }
-  pub fn create(&self, version: String) -> io::Result<u32> {
-    let id = rand::random::<u32>();
-    fs::create_dir(format!("{}/{}", self.folder, id))?;
-    fs::copy(
-      format!("server/{}/eula.txt", version),
-      format!("{}/{}/eula.txt", self.folder, id),
-    )?;
-    fs::copy(
-      format!("server/{}/eula.txt", version),
-      format!("{}/{}/server.jar",self.folder, id),
-    )?;
-    Ok(id)
+/// Creates a new MC server folder under the `server_files_folder` 
+/// # Arguments
+/// * `id` - The id for the server
+/// * `version` - The target version for the server.
+/// # Examples
+/// ```
+/// let manager = serbo::Manager::new("folder1","folder2");
+/// manager.create("1","1.16.1");
+/// ```
+/// # Remarks
+/// Returns a result that contains the id that was assigned to the server
+  pub fn create(&self, id: &str, version: &str) -> Result<(),Error> {
+    let target_folder = format!("{}/{}", self.server_files_folder, id);
+    let base_folder = format!("{}/{}", self.version_folder,version);
+    copy_dir(base_folder,target_folder)?;
+    Ok(())
   }
-  pub fn get(&mut self, id: String) -> Option<&mut Instance> {
-    self.servers.get_mut(&id)
+/// Returns an Option<t> containing a [Instance](struct.Instance.html) that represents the currently online server represented by the provided id
+/// # Arguments
+/// * `id` - The id that represents the requested server
+/// # Examples
+/// ```
+/// let manager = serbo::Manager::new("folder1","folder2");
+/// //Returns an Option
+/// let instance:serbo::Instance = manager.get("1").unwrap();
+/// ```
+/// # Remarks
+/// Queries the currently online servers, for get to return, must have been launched by calling [start](struct.Manager.html#method.start)
+  pub fn get(&mut self, id: &str) -> Option<&mut Instance> {
+    self.servers.get_mut(id)
   }
-  pub fn start(&mut self, id: String) -> Result<u32, Error> {
-    let mut rng = thread_rng();
-    let port = rng.gen_range(25565, 35565);
+/// Checks if server files exist for a given id
+/// # Arguments
+/// * `id` - The id that represents the requested server
+  pub fn exists(&mut self,id:&str) -> bool{
+    Path::new(&format!("{}/{}",self.server_files_folder,id)).exists()
+  }
+/// Checks if the server is online
+/// # Arguments
+/// * `id` - The id that represents the requested server
+/// # Remarks
+/// Queries the currently online servers, must have been launched by calling [start](struct.Manager.html#method.start)
+  pub fn is_online(&mut self, id:&str) -> bool{
+    match self.get(id){
+      Some(_) => true,
+      None => false
+    }
+  }
+/// Launches a server
+/// # Arguments
+/// * `id` - The id that represents the requested server
+/// * `port` - The port that the server should be started on
+  pub fn start(&mut self, id: &str, port:u32) -> Result<u32, Error> {
     let mut command = Command::new("java");
     command
       .stdin(Stdio::piped())
@@ -50,12 +163,12 @@ impl Manager {
         "-Xmx1024M",
         "-Xms1024M",
         "-jar",
-        "server.jar",
+        "fabric-server-launch.jar",
         "nogui",
         "--port",
         &port.to_string(),
       ])
-      .current_dir(format!("{}/{}", self.folder, id.to_string()));
+      .current_dir(format!("{}/{}", self.server_files_folder, id.to_string()));
     let child = command.spawn()?;
     let mut serv_inst = Instance {
       server_process: child,
@@ -90,11 +203,14 @@ impl Manager {
     });
     serv_inst.stdin_join = Some(stdin_thread_handle);
     serv_inst.stdout_join = Some(stdout_thread_handle);
-    &self.servers.insert(id, serv_inst);
+    &self.servers.insert(id.to_string(), serv_inst);
     Ok(port)
   }
-  pub fn stop(&mut self, id: String) -> io::Result<()> {
-    let serv = self.servers.get_mut(&id);
+/// Stops a server
+/// # Arguments
+/// * `id` - The id that represents the requested server
+  pub fn stop(&mut self, id: &str) -> io::Result<()> {
+    let serv = self.servers.get_mut(id);
     if let Some(inst) = serv{
       inst.stop()?;
       inst.stdout_join.take().unwrap().join();
@@ -102,22 +218,35 @@ impl Manager {
     }
     Ok(())
   }
-  pub fn delete(&mut self, id: String) -> io::Result<()> {
-    self.stop(id.clone())?;
-    fs::remove_dir_all(format!("{}/{}", &self.folder, id))?;
+/// Deletes a server's files
+/// # Arguments
+/// * `id` - The id that represents the requested server
+/// # Remarks
+/// Stops the server if it's currently running
+  pub fn delete(&mut self, id: &str) -> io::Result<()> {
+    self.stop(id)?;
+    fs::remove_dir_all(format!("{}/{}", &self.server_files_folder, id))?;
     Ok(())
   }
-  pub fn change_version(&mut self, id: String, target: String) -> Result<(), Error> {
-    self.stop(id.clone())?;
-    fs::remove_file(format!("{}/{}/server.jar", self.folder, id))?;
+/// Changes a server's version
+/// # Arguments
+/// * `id` - The id that represents the requested server
+/// * `target` - The target version to be switched to
+/// # Remarks
+/// Stops the server if it's currently running
+  pub fn change_version(&mut self, id: &str, target: &str) -> Result<(), Error> {
+    self.stop(id)?;
+    fs::remove_file(format!("{}/{}/server.jar", self.server_files_folder, id))?;
     fs::copy(
       format!("{}/{}/server.jar", self.version_folder, target),
-      format!("{}/{}/server.jar", self.folder, id),
+      format!("{}/{}/server.jar", self.server_files_folder, id),
     )?;
     Ok(())
   }
 }
 
+/// Represents a currently online server.
+/// Created by calling [start](struct.Manager.html#method.start) from a [Manager](struct.Manager.html)
 pub struct Instance {
   pub server_process: Child,
   stdout_join: Option<thread::JoinHandle<()>>,
@@ -128,15 +257,29 @@ pub struct Instance {
 }
 
 impl Instance {
+/// Stops the server, killing the server process and the stdin
+/// and stdout threads
   pub fn stop(&mut self) -> io::Result<()> {
     self.server_process.kill()?;
     Ok(())
   }
+/// Sends a string to the server stdin
+/// # Arguments
+/// * `msg` - A String that contains the message to be sent to the server.
+/// 
+/// # Remarks
+/// The message should not contain a trailing newline, as the send method handles it. 
   pub fn send(&mut self, msg: String) {
     let vec_lock = self.stdin_queue.clone();
     let mut vec = vec_lock.lock().unwrap();
     vec.push(msg);
   }
+//// Gets the output from server stdout
+///  # Arguments
+///  * `start` The line number of the first line that should be returned
+/// 
+/// # Remarks
+/// The returned Vec will contain the lines in the range of start to the end of output
   pub fn get(&self, start: u32) -> Vec<String> {
     let vec_lock = self.console_log.clone();
     let vec = vec_lock.lock().unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,70 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
-use rocket::request::{Form, FromForm};
-use rocket::State;
-use std::sync::Mutex;
-use std::io::Write;
-use rocket_contrib::json::Json;
-
 use serbo;
+use std::io::{self};
+use std::error::Error;
 
-#[macro_use]
-extern crate rocket;
-
-const valid_versions: [&'static str; 2] = ["1.15.2", "1.16.1"];
-
-#[derive(FromForm)]
-struct Target {
-    target_id: u32,
-    caller_id: u32,
-}
-
-#[derive(FromForm)]
-struct VersionTarget {
-    target_id: u32,
-    caller_id: u32,
-    target_version: String,
-}
-
-#[derive(FromForm)]
-struct CreateTarget {
-    caller_id: u32,
-    version:String
-}
-
-#[derive(FromForm)]
-struct ConsoleWriteTarget {
-    caller_id: u32,
-    target_id: u32,
-    msg: String
-}
-
-#[derive(FromForm)]
-struct ConsoleTarget {
-    caller_id: u32,
-    target_id: u32,
-    start_line: u32
-}
-
-struct StateStruct{
-    servers: Mutex<serbo::Manager>
-}
-
-#[post("/writeConsole", data = "<target>")]
-fn _write(target: Form<ConsoleWriteTarget>, state:State<StateStruct>) -> String{
-    if let Some(instance) = state.servers.lock().unwrap().get(target.target_id.to_string()){
-        instance.send(target.msg.clone());
-        return String::from("1");
+fn main() -> Result<(), Box<dyn Error>>{
+  let mut manager = serbo::Manager::new("servers", "versions");
+  let port = 25565;
+  let id = "1";
+  if !manager.exists(id){
+    manager.create(id, "1.16.1-fabric")?;
+  }
+  loop {
+    let reader = io::stdin();
+    let mut buf = String::new();
+    println!("Enter your command.");
+    reader.read_line(&mut buf)?;
+    match buf.trim() {
+      "change_version" => {
+        let mut send_buf = String::new();
+        println!("Enter the version to change to.");
+        reader.read_line(&mut send_buf)?;
+        //Remove the newline from read_line
+        send_buf = send_buf[..send_buf.chars().count()-1].to_string();
+        manager.change_version(id,&send_buf)?;
+      },
+      "stop" => {
+        //Stops the server
+        println!("Server stopping.");
+        manager.stop(id)?;
+        break Ok(());
+      }
+      "start" => {
+        //Starts the server
+        println!("Server starting.");
+        manager.start(id, port)?;
+      }
+      "send" => {
+        //Prompts for a command to send to the server
+        let mut send_buf = String::new();
+        println!("Enter the command to send to the server.");
+        reader.read_line(&mut send_buf)?;
+        //Remove the newline from read_line
+        send_buf = send_buf[..send_buf.chars().count()-1].to_string();
+        let instance = manager.get(id).unwrap();
+        instance.send(send_buf);
+      },
+      "get" => {
+        //Gets the last 5 stdout lines
+        let instance:&serbo::Instance = manager.get(id).unwrap();
+        let vec = instance.get(0);
+        let length = vec.len();
+        //Create a vec from the last 5 lines
+        let trimmed_vec;
+        if length >= 5{
+         trimmed_vec = Vec::from(&vec[length-5..]);
+        }
+        else{
+          trimmed_vec = Vec::from(vec);
+        }
+        for line in trimmed_vec{
+          println!("{}",line);
+        }
+      },
+      _ => {
+        println!("Unrecognized command");
+      }
     }
-    String::from("-1")
-}
-
-#[post("/version", data = "<target>")]
-fn version(target: Form<VersionTarget>, state:State<StateStruct>) -> String {
-    match state.servers.lock().unwrap().change_version(target.target_id.to_string(),target.target_version.clone()){
-        Ok(_) => String::from("1"),
-        Err(e) => String::from("-1")
   }
 }
 
-#[post("/stop", data = "<target>")]
-fn stop(target: Form<Target>, state:State<StateStruct>) -> String {
-    match state.servers.lock().unwrap().stop(target.target_id.to_string()){
-        Ok(_) => String::from("1"),
-        Err(e) => String::from("-1")
-  }
-}
-
-#[post("/getConsole", data="<target>")]
-fn get_console(target: Form<ConsoleTarget>, state:State<StateStruct>) -> Json<Vec<String>>{
-    if let Some(instance) = state.servers.lock().unwrap().get(target.target_id.to_string()){
-        return Json(instance.get(target.start_line))
-    }
-    Json(Vec::new())
-}
-
-#[post("/start", data = "<target>")]
-fn start(target: Form<Target>, state:State<StateStruct>) -> String {
-  match state.servers.lock().unwrap().start(target.target_id.to_string()){
-    Ok(_) => String::from("1"),
-    Err(e) => String::from("-1")
-  }
-}
-
-#[post("/delete", data = "<target>")]
-fn delete(target: Form<Target>, state:State<StateStruct>) -> String {
-  match state.servers.lock().unwrap().delete(target.target_id.to_string()){
-      Ok(_) => String::from("1"),
-      Err(e) => String::from("-1")
-  }
-}
-
-#[post("/create", data = "<target>")]
-fn create(target: Form<CreateTarget>,state:State<StateStruct>) -> String {
-    match state.servers.lock().unwrap().create(target.version.clone()){
-        Ok(port) => String::from(port.to_string()),
-        Err(e) => String::from("-1")
-    }
-}
-
-fn main() {
-    let state = StateStruct{
-        servers:Mutex::new(serbo::Manager::new(String::from("servers"),String::from("server")))
-    };
-    rocket::ignite()
-        .manage(state)
-        .mount("/", routes![create, start, stop, delete, version,get_console,_write])
-        .launch();
-}
