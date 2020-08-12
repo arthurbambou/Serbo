8e8e2afe0decfa9308565d1f8c5289dc6e44f7fc
Reed Krawiec <reedkrawiec@gmail.com>
2020-08-11 20:54:23 -0400

    Improved crash detection

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,6 @@
 //!        //Stops the server
 //!        println!("Server stopping.");
 //!        manager.stop(id)?;
-//!        break Ok(());
 //!      }
 //!      "start" => {
 //!        //Starts the server
@@ -50,33 +49,41 @@
 //!      }
 //!      "send" => {
 //!        //Prompts for a command to send to the server
-//!        let instance = manager.get(id);
-//!        match instance {
-//!          Some(i) => {
-//!            let mut send_buf = String::new();
-//!            println!("Enter the command to send to the server.");
-//!            reader.read_line(&mut send_buf)?;
-//!            //Remove the newline from read_line
-//!            send_buf = send_buf[..send_buf.chars().count() - 1].to_string();
-//!            i.send(send_buf)?;
-//!          }
-//!          None => println!("Server offline."),
+//!        if let Some(instance) = manager.get(id){
+//!          let mut send_buf = String::new();
+//!          println!("Enter the command to send to the server.");
+//!          reader.read_line(&mut send_buf)?;
+//!          //Remove the newline from read_line
+//!          send_buf = send_buf[..send_buf.chars().count() - 1].to_string();
+//!          match instance.send(send_buf){
+//!            Ok(()) => println!("Command sent."),
+//!            Err(a) => {
+//!              println!("{}",a);
+//!            }
+//!          };
+//!        }
+//!        else{
+//!          println!("Server Offline.");
 //!        }
 //!      }
 //!      "get" => {
 //!        //Gets the last 5 stdout lines
-//!        let instance: &serbo::Instance = manager.get(id).unwrap();
-//!        let vec = instance.get(0);
-//!        let length = vec.len();
-//!        //Create a vec from the last 5 lines
-//!        let trimmed_vec;
-//!        if length >= 5 {
-//!          trimmed_vec = Vec::from(&vec[length - 5..]);
-//!        } else {
-//!          trimmed_vec = Vec::from(vec);
+//!        if let Some(instance) = manager.get(id){
+//!          let vec = instance.get(0);
+//!          let length = vec.len();
+//!          //Create a vec from the last 5 lines
+//!          let trimmed_vec;
+//!          if length >= 10 {
+//!            trimmed_vec = Vec::from(&vec[length - 10..]);
+//!          } else {
+//!            trimmed_vec = Vec::from(vec);
+//!          }
+//!          for line in trimmed_vec {
+//!            println!("{}", line);
+//!          }
 //!        }
-//!        for line in trimmed_vec {
-//!          println!("{}", line);
+//!        else {
+//!          println!("Server Offline.")
 //!        }
 //!      }
 //!      _ => {
@@ -101,29 +108,37 @@ use std::time;
 
 type Result<T> = std::result::Result<T, Error>;
 
-
 #[derive(Debug)]
 pub enum Error {
   /// Arises when there is an error regarding IO
   IoError(std::io::Error),
   /// Arises when an offline server is attempted to be used
   ServerOffline(String),
+  /// Arises when attempting to start a server that is already online
+  ServerAlreadyOnline(String),
   /// Arises when a server's files are missing
   ServerFilesMissing(String),
   /// Arises when attempting to create a server with the same id as an existing server
   ServerAlreadyExists(String),
   /// Arises when there is an error involving a server's stdin/stdout threads
-  ThreadError(String)
+  ThreadError(String, String),
+  /// Arises when the server processes needs to be referenced, but has unexpectedly ended.
+  /// May occur due to the server process being killed, the server crashing or ingame methods
+  /// to stop the server
+  ServerProcessExited(String),
+
 }
 
-impl std::error::Error for Error{
-  fn description(&self) -> &str{
-    match *self{
+impl std::error::Error for Error {
+  fn description(&self) -> &str {
+    match *self {
       Error::IoError(_) => "IOError",
       Error::ServerFilesMissing(_) => "MissingServer",
       Error::ServerOffline(_) => "ServerOffline",
       Error::ServerAlreadyExists(_) => "ServerAlreadyExists",
-      Error::ThreadError(_) => "ThreadError"
+      Error::ThreadError(_, _) => "ThreadError",
+      Error::ServerProcessExited(_) => "ServerProcessExited",
+      Error::ServerAlreadyOnline(_) => "ServerAlreadyOnline"
     }
   }
 }
@@ -131,17 +146,25 @@ impl std::error::Error for Error{
 impl fmt::Display for Error {
   fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
     match *self {
-      Error::IoError(ref a) => write!(f,"Io error: {}",a),
-      Error::ServerFilesMissing(ref a) => write!(f,"Server missing with id:{}",a),
-      Error::ServerOffline(ref a) => write!(f,"Server offline with id:{}",a),
-      Error::ServerAlreadyExists(ref a) => write!(f,"Server already exists with id:{}",a),
-      Error::ThreadError(ref a) => write!(f,"Error while creating stdin/stdout threads for id:{}",a)
+      Error::IoError(ref a) => write!(f, "Io error: {}", a),
+      Error::ServerFilesMissing(ref a) => write!(f, "Server files not found for id:{}", a),
+      Error::ServerOffline(ref a) => write!(f, "Server with id:{} offline while called.", a),
+      Error::ServerAlreadyExists(ref a) => write!(f, "Server files already exists with id:{}", a),
+      Error::ThreadError(ref a, ref b) => {
+        write!(f, "Error while creating {} thread for server id:{}", a, b)
+      }
+      Error::ServerProcessExited(ref a) => {
+        write!(f, "Server processes needed for id:{}, but has unexpectedly exited.",a)
+      },
+      Error::ServerAlreadyOnline(ref a) => {
+        write!(f,"Attempted to start already online server with id:{}",a)
+      }
     }
   }
 }
 
-impl From<std::io::Error> for Error{
-  fn from(e:std::io::Error) -> Self{
+impl From<std::io::Error> for Error {
+  fn from(e: std::io::Error) -> Self {
     Error::IoError(e)
   }
 }
@@ -151,7 +174,7 @@ pub struct Manager {
   servers: HashMap<String, Instance>,
   server_files_folder: String,
   version_folder: String,
-  jar_name: String
+  jar_name: String,
 }
 
 impl Manager {
@@ -165,12 +188,12 @@ impl Manager {
   /// ```
   /// # Remarks
   /// The version_folder should be a folder that contains folders that are named the same as the MC server files they contain.
-  pub fn new(server_files_folder: &str, version_folder: &str, jar_name:&str) -> Manager {
+  pub fn new(server_files_folder: &str, version_folder: &str, jar_name: &str) -> Manager {
     Manager {
       servers: HashMap::new(),
       server_files_folder: server_files_folder.to_string(),
       version_folder: version_folder.to_string(),
-      jar_name: jar_name.to_string()
+      jar_name: jar_name.to_string(),
     }
   }
   /// Creates a new MC server folder under the `server_files_folder`
@@ -186,7 +209,7 @@ impl Manager {
   /// # Remarks
   /// Returns a result that contains the id that was assigned to the server
   pub fn create(&mut self, id: &str, version: &str) -> Result<()> {
-    if self.exists(id){
+    if self.exists(id) {
       return Err(Error::ServerAlreadyExists(id.to_string()));
     }
     let target_folder = format!("{}/{}", self.server_files_folder, id);
@@ -206,7 +229,16 @@ impl Manager {
   /// # Remarks
   /// Queries the currently online servers, for get to return, must have been launched by calling [start](struct.Manager.html#method.start)
   pub fn get(&mut self, id: &str) -> Option<&mut Instance> {
-    self.servers.get_mut(id)
+    match self.servers.get_mut(id) {
+      Some(a) => match a.is_valid() {
+        Ok(b) => match b {
+          true => Some(a),
+          false => None,
+        },
+        Err(_) => None,
+      },
+      None => None,
+    }
   }
   /// Checks if server files exist for a given id
   /// # Arguments
@@ -230,68 +262,75 @@ impl Manager {
   /// * `id` - The id that represents the requested server
   /// * `port` - The port that the server should be started on
   pub fn start(&mut self, id: &str, port: u32) -> Result<u32> {
-    
-    if !self.exists(id){
+    if !self.exists(id) {
       return Err(Error::ServerFilesMissing(id.to_string()));
     }
-    
-    let mut command = Command::new("java");
-    command
-      .stdin(Stdio::piped())
-      .stdout(Stdio::piped())
-      .args(&[
-        "-Xmx1024M",
-        "-Xms1024M",
-        "-jar",
-        &self.jar_name,
-        "nogui",
-        "--port",
-        &port.to_string(),
-      ])
-      .current_dir(format!("{}/{}", self.server_files_folder, id.to_string()));
-    let child = command.spawn()?;
-    let mut serv_inst = Instance {
-      server_process: child,
-      stdout_join: None,
-      stdin_join: None,
-      console_log: Arc::new(Mutex::new(Vec::new())),
-      stdin_queue: Arc::new(Mutex::new(Vec::new())),
-      port: port,
-      id: id.to_string()
-    };
-    let stdout = match serv_inst.server_process.stdout.take(){
-      Some(e) => e,
-      None => return Err(Error::ThreadError(id.to_string()))
-    };
-    let stdin = match serv_inst.server_process.stdin.take(){
-      Some(e) => e,
-      None => return Err(Error::ThreadError(id.to_string()))
-    };
-    let stdout_arc = serv_inst.console_log.clone();
-    let stdin_arc = serv_inst.stdin_queue.clone();
-    let stdout_thread_handle = thread::spawn(move || {
-      let reader = BufReader::new(stdout).lines();
-      reader.filter_map(|line| line.ok()).for_each(|line| {
-        let mut lock = stdout_arc.lock().unwrap();
-        lock.push(line);
-      });
-    });
-    let stdin_thread_handle = thread::spawn(move || {
-      let mut writer = BufWriter::new(stdin);
-      loop {
-        let mut vec = stdin_arc.lock().unwrap();
-        vec.drain(..).for_each(|x| {
-          writeln!(writer, "{}", x);
-          writer.flush();
+
+    if let Some(_) = self.servers.get(id) {
+      Err(Error::ServerAlreadyOnline(id.to_string()))
+    } else {
+      let mut command = Command::new("java");
+      command
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .args(&[
+          "-Xmx1024M",
+          "-Xms1024M",
+          "-jar",
+          &self.jar_name,
+          "nogui",
+          "--port",
+          &port.to_string(),
+        ])
+        .current_dir(format!("{}/{}", self.server_files_folder, id.to_string()));
+      let child = command.spawn()?;
+      let mut serv_inst = Instance {
+        server_process: child,
+        stdout_join: None,
+        stdin_join: None,
+        console_log: Arc::new(Mutex::new(Vec::new())),
+        stdin_queue: Arc::new(Mutex::new(Vec::new())),
+        port: port,
+        id: id.to_string(),
+      };
+      let stdout = match serv_inst.server_process.stdout.take() {
+        Some(e) => e,
+        None => return Err(Error::ThreadError("stdout".to_string(), id.to_string())),
+      };
+      let stdin = match serv_inst.server_process.stdin.take() {
+        Some(e) => e,
+        None => return Err(Error::ThreadError("stdin".to_string(), id.to_string())),
+      };
+
+      let stdout_arc = serv_inst.console_log.clone();
+      let stdin_arc = serv_inst.stdin_queue.clone();
+
+      let stdout_thread_handle = thread::spawn(move || {
+        let reader = BufReader::new(stdout).lines();
+        reader.filter_map(|line| line.ok()).for_each(|line| {
+          let mut lock = stdout_arc.lock().unwrap();
+          lock.push(line);
         });
-        drop(vec);
-        sleep(time::Duration::from_secs(2));
-      }
-    });
-    serv_inst.stdin_join = Some(stdin_thread_handle);
-    serv_inst.stdout_join = Some(stdout_thread_handle);
-    &self.servers.insert(id.to_string(), serv_inst);
-    Ok(port)
+      });
+
+      let stdin_thread_handle = thread::spawn(move || {
+        let mut writer = BufWriter::new(stdin);
+        loop {
+          let mut vec = stdin_arc.lock().unwrap();
+          vec.drain(..).for_each(|x| {
+            writeln!(writer, "{}", x);
+            writer.flush();
+          });
+          drop(vec);
+          sleep(time::Duration::from_secs(2));
+        }
+      });
+      serv_inst.stdin_join = Some(stdin_thread_handle);
+      serv_inst.stdout_join = Some(stdout_thread_handle);
+      &self.servers.insert(id.to_string(), serv_inst);
+      Ok(port)
+    }
   }
   /// Stops a server
   /// # Arguments
@@ -300,8 +339,10 @@ impl Manager {
     let serv = self.servers.get_mut(id);
     if let Some(inst) = serv {
       inst.stop()?;
+      println!("WWW");
       inst.stdout_join.take().unwrap().join();
       inst.stdin_join.take().unwrap().join();
+      println!("DDD");
       return Ok(());
     }
     Err(Error::ServerOffline(id.to_string()))
@@ -313,7 +354,7 @@ impl Manager {
   /// Stops the server if it's currently running
   pub fn delete(&mut self, id: &str) -> Result<()> {
     let _ = self.stop(id);
-    if !self.exists(id){
+    if !self.exists(id) {
       return Err(Error::ServerFilesMissing(id.to_string()));
     }
     fs::remove_dir_all(format!("{}/{}", &self.server_files_folder, id))?;
@@ -327,7 +368,7 @@ impl Manager {
   /// Stops the server if it's currently running
   pub fn change_version(&mut self, id: &str, target: &str) -> Result<()> {
     let _ = self.stop(id);
-    if !self.exists(id){
+    if !self.exists(id) {
       return Err(Error::ServerFilesMissing(id.to_string()));
     }
     fs::remove_file(format!("{}/{}/server.jar", self.server_files_folder, id))?;
@@ -348,15 +389,29 @@ pub struct Instance {
   console_log: Arc<Mutex<Vec<String>>>,
   stdin_queue: Arc<Mutex<Vec<String>>>,
   pub port: u32,
-  pub id: String
+  pub id: String,
 }
 
 impl Instance {
   /// Stops the server, killing the server process and the stdin
   /// and stdout threads
   pub fn stop(&mut self) -> Result<()> {
-    self.server_process.kill()?;
+    let _ = self.process_check();
+    self.send(String::from("/stop"))?;
     Ok(())
+  }
+  /// Checks if the server process is still valid (has not crashed or exited).
+  pub fn is_valid(&mut self) -> Result<bool> {
+    match self.server_process.try_wait()? {
+      Some(_) => Ok(false),
+      None => Ok(true),
+    }
+  }
+  fn process_check(&mut self) -> Result<()> {
+    match self.is_valid()? {
+      true => Ok(()),
+      false => Err(Error::ServerProcessExited(self.id.clone())),
+    }
   }
   /// Sends a string to the server stdin
   /// # Arguments
@@ -365,6 +420,7 @@ impl Instance {
   /// # Remarks
   /// The message should not contain a trailing newline, as the send method handles it.
   pub fn send(&mut self, msg: String) -> Result<()> {
+    let _ = self.process_check()?;
     let vec_lock = self.stdin_queue.clone();
     let mut vec = vec_lock.lock().unwrap();
     vec.push(msg);
